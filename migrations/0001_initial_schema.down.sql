66a6fd5ca98205acd7931514140965f77d0e5492
Terry Drozdowski <terry.drozdowski@gametime.co>
2024-08-06 14:06:17 -0700

    Initial commit

---
+++ migrations/0001_initial_schema.down.sql
@@ -0,0 +1,4 @@
+drop table if exists invoices_items;
+drop table if exists invoices;
+drop table if exists item;
+drop table if exists persons;
---
+++ migrations/0001_initial_schema.up.sql
@@ -0,0 +1,29 @@
+create table persons (
+    id serial primary key,
+    alt_id uuid unique gen_random_uuid(),
+    name text not null,
+    email text unique not null,
+    created_at timestamp now(),
+    last_update timestamp now()
+);
+
+create table if not exists invoices (
+    id serial primary key,
+    user_id int not null,
+    total decimal not null,
+    paid boolean default false,
+    created_at timestamp not null
+);
+
+create table item (
+  id serial primary key,
+  name varchar(255) not null,
+  description text not null,
+  unit_price decimal not null,
+  created_at timestamp not null
+)
+
+create table if not exists invoices_items (
+    invoice_id int not null,
+    item_id int not null
+);
