afe357ab4f399525a3f3ba5d2ce7eaabe7b981a8
terry <terry@xymox.dev>
2025-04-19 22:52:59 -0700

    Add Pyroscope profiling dependencies and update crate versions
    Added `pyroscope` and `pyroscope_pprofrs` crates for performance profiling. Multiple dependency versions were updated, including `syn` to 2.0.90, `rand`, `thiserror`, and `opentelemetry` crates to improve compatibility and functionality.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use opentelemetry::trace::TracerProvider as _;
 use opentelemetry::KeyValue;
 use opentelemetry_otlp::WithExportConfig;
 use opentelemetry_sdk::Resource;
+use pyroscope::PyroscopeAgent;
+use pyroscope_pprofrs::{pprof_backend, PprofConfig};
 use tracing::info;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
@@ -19,19 +21,19 @@ async fn init() {
     let otlp_endpoint =
         std::env::var("OTLP_ENDPOINT").unwrap_or_else(|_| "http://localhost:4317".to_string());
     let log_endpoint = otlp_endpoint.clone();
-    let provider = opentelemetry_sdk::trace::TracerProvider::builder()
-        .with_batch_exporter(
-            opentelemetry_otlp::SpanExporter::builder()
-                .with_tonic()
-                .with_endpoint(otlp_endpoint)
-                .build()
-                .expect("Failed to create exporter"),
-            opentelemetry_sdk::runtime::Tokio,
+    let exporter = opentelemetry_otlp::SpanExporter::builder()
+        .with_tonic()
+        .with_endpoint(otlp_endpoint)
+        .build()
+        .expect("Failed to create exporter");
+
+    let provider = opentelemetry_sdk::trace::TracerProviderBuilder::default()
+        .with_batch_exporter(exporter)
+        .with_resource(
+            Resource::builder()
+                .with_attribute(KeyValue::new("service.name", "inventory_service"))
+                .build(),
         )
-        .with_resource(Resource::new(vec![KeyValue::new(
-            "resource.service.name",
-            "inventory_service",
-        )]))
         .build();
 
     let tracer = provider.tracer("inventory-service");
@@ -47,5 +49,18 @@ async fn init() {
         .with(tracing_subscriber::fmt::layer())
         .with(otel_layer)
         .init();
+
+    // setup profiling
+    // Configure profiling backend
+    let pprof_config = PprofConfig::new().sample_rate(100);
+    let backend_impl = pprof_backend(pprof_config);
+
+    // Configure Pyroscope Agent
+    let pyro_endpoint = std::env::var("PYROSCOPE_ENDPOINT")
+        .unwrap_or_else(|_| "http://pyroscope-ingester.pyroscope:4040".to_string());
+    let agent = PyroscopeAgent::builder(pyro_endpoint.as_str(), "inventory_service")
+        .backend(backend_impl)
+        .build()
+        .expect("Failed to create Pyroscope agent");
     info!("Server initialization started...");
 }
